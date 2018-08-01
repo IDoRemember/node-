786d25394c14111655dadfe8afb7bc35e6bf0a41
Shirly Chen <1015885915@qq.com>
2018-08-01 19:18:46 +0800

    房产爬虫+使用babel将es6转es5

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var pachong = require('./routes/pachong');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var file = require('./routes/file');
@@ -27,6 +27,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/yamlToJson', yamlToJson);
 app.use('/file', file);
+app.use('/pachong',pachong.gethouse)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
--- routes/yamlToJson.js
+++ routes/yamlToJson.js
@@ -6,7 +6,7 @@ var multer = require('multer')
 var upload = multer({ dest: 'uploads/' })
 
 // file为文件所在路径
-/* GET users listing. */
+/* 将yaml文件转成json */
 router.get('/', function (req, res, next) {
   var data = YAML.parse(fs.readFileSync('/Users/shirlychen/practice/nodeProject/node-/data/imgSize.yaml').toString());
   var imgSizes = {};
@@ -29,18 +29,4 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'my home' });
 });
 
-// router.post('/upload',function(req,res,next) {
-//   console.log(req.body)
-//   var data = YAML.parse(fs.readFileSync('/Users/shirlychen/practice/nodeProject/node-/data/imgSize.yaml').toString());
-//   console.log(data)
-// })
-// router.post("/upload", upload.single('file'),(req, res) => {
-
-//   // req.file 是 'file' 文件的信息 （前端传递的文件类型在req.file中获取）
-//   // req.body 将具有文本域数据，如果存在的话  。（上面前端代码中传递的date字段在req.body中获取）
-//   console.log(req) //{ date: '2018/1/20 下午5:25:56' }
-
-//   // ---------- 因为保存的文件为二进制，取消下面代码块注释可让保存的图片文件在本地文件夹中预览 ------
-
-// })
 module.exports = router;
